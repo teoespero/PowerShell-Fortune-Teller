fae9b23f093157f170e1950ec24705d0cec9937e
Teo Espero <38012835+teoespero@users.noreply.github.com>
2019-07-31 15:35:02 -0700

    Some minimal code changes

--- fortune-teller.ps1
+++ fortune-teller.ps1
@@ -13,18 +13,19 @@
 
 ## declare our vars
 
-$question = ""    # This variable will store the player's question
-$status = "Play"  # This variable will be used to control game termination
-$answer = 0       # This variable stores a randomly generated number
+$question = ""    ## This variable will store the player's question
+$status = "Play"  ## This variable will be used to control game termination
+$answer = 0       ## This variable stores a randomly generated number
 
-$randomNo = New-Object System.Random # This variable stores a random object
-$time = (Get-Date).Hour # This variable stores the current hour of the day
+$randomNo = New-Object System.Random ## This variable stores a random object
+$time = (Get-Date).Hour ## This variable stores the current hour of the day
 
 ## display the game's opening screen
 
 Clear-Host
 
-#Display the game's opening screen
+## display the game's opening screen
+
 Write-Host
 Write-Host
 Write-Host "           ==================================================================="
@@ -85,9 +86,11 @@ Write-Host " Press Enter to continue."
 ## Pause the script execution and wait for the player to press the Enter key
 Read-Host
 
-#Continue gameplay until the player decides to stop
+
+## Continue gameplay until the player decides to stop
+
 while ($status -ne "Stop") {
-    #Ask the player the first question
+    ## Ask the player the first question
 
     while ($question -eq ""){
         Clear-Host 
@@ -97,12 +100,12 @@ while ($status -ne "Stop") {
     }
     $question = "" #Reset variable to an empty string
  
-    #Retrieve a random number between 1 and 4
+    ## Retrieve a random number between 1 and 4
     $answer = $randomNo.Next(1,5)
 
-    #Select an answer based on the time and the random number
+    ## Select an answer based on the time and the random number
 
-    #If it is the afternoon the fortune teller will be a little cranky
+    ## If it is the afternoon the fortune teller will be a little cranky
     if ($time -gt 12) {
         Write-Host
         if ($answer -eq 1) { " Grrrr. The answer is no!" }
@@ -110,7 +113,8 @@ while ($status -ne "Stop") {
         if ($answer -eq 3) { " Grrrr. The answer is unclear!" }
         if ($answer -eq 4) { " Grrrr. The answer is yes!" }
     }
-    #If it is morning, the fortune teller will be in a good mood
+
+    ## If it is morning, the fortune teller will be in a good mood
     else {
         Write-Host
         if ($answer -eq 1) { " Ah. The answer is yes!" }
@@ -141,18 +145,25 @@ while ($status -ne "Stop") {
     Write-Host " Press Enter to continue."
 
     ## Pause script execution and wait for the player to press the Enter key
+
     Read-Host
-    ## Clear the Windows command console screen
 
     Clear-host
+
+
     Write-Host
-    #Prompt the player to continue or quit
+    
+    ## Prompt the player to continue or quit
+
     $reply = read-host " Press Enter to ask another question or type Q to quit."
+
+    ## note that Q and q are treated as the same, -eq as designed is not 
+    ## case sensitive 
+
     if ($reply -eq "q") { $status = "Stop" }
 }
 
 
-#################################################################
 ## Show credits 
 
 Clear-Host
@@ -179,9 +190,9 @@ Write-Host
 
 ## create a 5-second delay before returning the player to the command shell
 
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 3
 
 Clear-Host
 
-#################################################################
+##################################################################################################
 ## EOF 
